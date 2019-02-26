4670465b7db90bf2fb3c074af4032525a08256a2
Tama Owens <30021963@student.toiohomai.ac.nz>
2019-02-27 10:49:07 +1300

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("please enter your age: ");
+            /*below is how we change the code from a string*/
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter your name: ");
+
+            name = (Console.ReadLine();
+       
+           
+            Console.WriteLine(name + age);
+            Console.ReadLine();
+
+      
+
+        }
+    }
+}
---
+++ Tamasapp/Tamasapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Name of project*/
+namespace Tamasapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*created write of program saying Hello Tama*/
+            Console.WriteLine("Hello Tama");
+            /*takes everything that is inputted and takes it in as a big collection Strings */ 
+            Console.ReadLine();
+        }
+       
+    }
+}
